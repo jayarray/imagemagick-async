1170895e20af52dd32790a611ac0d321a7406dd5
Jesus Arevalo <isa90201@gmail.com>
2018-03-24 21:55:48 -0700

    Added API file

---
+++ api.js
@@ -0,0 +1,7 @@
+exports.Canvas = require('./canvas.js');
+exports.Circle = require('./circle.js');
+exports.Color = require('./color.js');
+exports.Coordinates = require('./coordinates.js');
+exports.Line = require('./line.js');
+exports.Path = require('./path.js');
+exports.Point = require('./point.js');
