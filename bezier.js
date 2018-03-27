f3c2356c86305976c34295ceb8bd37b18b9c59f3
Jesus Arevalo <isa90201@gmail.com>
2018-03-27 14:08:01 -0700

    Moved all bezier related classes and functions intot heir own file.

---
+++ bezier.js
@@ -0,0 +1,103 @@
+let VALIDATE = require('./validate.js');
+let LOCAL_COMMAND = require('linux-commands-async').Command.LOCAL;
+
+//---------------------------------
+// CONSTANTS
+
+const WIDTH_MIN = 1;
+
+//---------------------------------
+// ELLIPSE
+
+class Bezier {
+  constructor(points, strokeColor, strokeWidth, fillColor) {
+    this.points_ = points;
+    this.strokeColor_ = strokeColor;
+    this.strokeWidth_ = strokeWidth;
+    this.fillColor_ = fillColor;
+  }
+
+  /** 
+   * @returns {string} Returns a space-delimited string representing all points in the bezier curve.
+   */
+  PointsToString() {
+    return this.points_.map(p => p.String()).join(' ');
+  }
+
+  /** 
+   * @returns {Array<string|number>} Returns an array of arguments.
+  */
+  Args() {
+    return [
+      '-fill', this.fillColor_,
+      '-stroke', this.strokeColor_,
+      '-strokewidth', this.strokeWidth_,
+      '-draw', `bezier ${this.PointsToString()}`
+    ];
+  }
+
+  /**
+   * Create a Bezier object with the specified properties.
+   * @param {Array<Coordinates>} points A list of points for the bezier curve to travel through.
+   * @param {string} strokeColor The color of the line connecting all the points. (Valid color format string used in Image Magick)
+   * @param {number} strokeWidth Width of the line connecting all the points. (Larger values produce thicker lines.)
+   * @param {string} fillColor The color to fill the bezier. (Valid color format string used in Image Magick)
+   */
+  static Create(points, strokeColor, strokeWidth, fillColor) {
+    let error = VALIDATE.IsArray(points);
+    if (error)
+      return Promise.reject(`Failed to create bezier curve: points is ${error}`);
+
+    error = VALIDATE.IsStringInput(strokeColor);
+    if (error)
+      return Promise.reject(`Failed to create bezier curve: stroke color is ${error}`);
+
+    error = VALIDATE.IsInteger(strokeWidth);
+    if (error)
+      return Promise.reject(`Failed to create bezier curve: stroke width is ${error}`);
+
+    error = VALIDATE.IsIntegerInRange(strokeWidth, WIDTH_MIN, null);
+    if (error)
+      return Promise.reject(`Failed to create bezier curve: stroke width is ${error}`);
+
+    error = VALIDATE.IsStringInput(fillColor);
+    if (error)
+      return Promise.reject(`Failed to create bezier curve: fill color is ${error}`);
+
+    return Promise.resolve(new Bezier(points, strokeColor, strokeWidth, fillColor));
+  }
+}
+
+/**
+ * Render a bezier curve to the specified destination.
+ * @param {Canvas} canvas Canvas object
+ * @param {Bezier} bezier Ellipse object
+ * @param {string} dest Destination
+ */
+function Draw(canvas, bezier, dest) {
+  if (canvas.constructor.name != 'Canvas')
+    return Promise.reject(`Failed to draw bezier curve: canvas is invalid type.`);
+
+  if (ellipse.constructor.name != 'Ellipse')
+    return Promise.reject(`Failed to draw bezier curve: bezier is invalid type.`);
+
+  let error = VALIDATE.IsStringInput(dest);
+  if (error)
+    return Promise.reject(`Failed to draw bezier curve: dest is ${error}`);
+
+  return new Promise((resolve, reject) => {
+    LOCAL_COMMAND.Execute('convert', canvas.Args().concat(bezier.Args()).concat(dest)).then(output => {
+      if (output.stderr) {
+        reject(`Failed to draw bezier curve: ${output.stderr}`);
+        return;
+      }
+      resolve();
+    }).catch(error => `Failed to draw bezier curve: ${error}`);
+  });
+}
+
+//----------------------------
+// EXPORTS.
+
+exports.Create = Ellipse.Create;
+exports.Draw = Draw;
