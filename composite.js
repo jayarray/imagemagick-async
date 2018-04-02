496eca75c8ca645a4b2dfb81c94362c6c4716927
Jesus Arevalo <isa90201@gmail.com>
2018-04-02 13:45:52 -0700

    Added more error checks for Draw function. Completed draw command.

--- composite.js
+++ composite.js
@@ -2,20 +2,30 @@ let VALIDATE = require('./validate.js');
 let LOCAL_COMMAND = require('linux-commands-async').Command.LOCAL;
 
 //------------------------------------------
+// CONSTANTS
+
+const MIN_FILEPATHS = 2;
+
+//-------------------------------------------
+// DRAW
 
 /**
  * Render a composite image made up of the provided lists of filepaths. NOTE: Images are layered from bottom to top.
  * @param {Array<string>} filepaths A list of image filepaths.
- * @param {string} gravity Gravity
+ * @param {string} gravity (Optional) Gravity
  * @param {string} outputPath The path where the image will be rendered to.
+ * @returns {Promise} Returns a promise that resolves if successful. Otherwise, it returns an error.
  */
 function Draw(filepaths, gravity, outputPath) {
   let error = VALIDATE.IsArray(filepaths);
   if (error)
     return Promise.reject(`Failed to draw composite image: filepaths are ${error}`);
 
+  if (filepaths.length < MIN_FILEPATHS)
+    return Promise.reject(`Failed to draw composite image: ${MIN_FILEPATHS} or more filepaths are required`);
+
   error = VALIDATE.IsStringInput(gravity);
-  if (error)
+  if (gravity != null && error)
     return Promise.reject(`Failed to draw composite image: gravity is ${error}`);
 
   error = VALIDATE.IsStringInput(outputPath);
@@ -23,17 +33,32 @@ function Draw(filepaths, gravity, outputPath) {
     return Promise.reject(`Failed to draw composite image: output path is ${error}`);
 
   return new Promise((resolve, reject) => {
-    let args
-  });
+    let args = [];
 
-  let cmd = ' convert -gravity ' + gravity + ' ' + filepaths[0] + ' ' + filepaths[1];
-  if (filepaths.length > 2) {
+    // Add gravity
+    if (gravity)
+      args.push('-gravity', gravity);
+    args.push(outputPath);
+
+    // Add first 2 paths
+    args.push(filepaths[0], filepaths[1]);
+
+    // Add other parts accordingly
     for (let i = 2; i < filepaths.length; ++i) {
-      cmd += ' -composite ' + filepaths[i];
+      args.push('-composite', filepaths[i]);
     }
-  }
-  cmd += ' -composite ' + dest;
-  return execute(cmd);
+
+    // Add output path
+    args.push('-composite', outputPath);
+
+    LOCAL_COMMAND.Execute('convert', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to draw composite image: ${output.stderr}`);
+        return;
+      }
+      resolve();
+    }).catch(error => `Failed to draw composite image: ${error}`);
+  });
 }
 
 //---------------------------------------
