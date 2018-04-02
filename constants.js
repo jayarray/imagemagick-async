9218daca306daf2f8d8234985d8e443cbe98d987
Jesus Arevalo <isa90201@gmail.com>
2018-04-02 14:19:54 -0700

    Added file for constant values.

---
+++ constants.js
@@ -0,0 +1,16 @@
+const DISPOSE_VALUES = [
+  'Undefined',
+  'None',
+  'Previous',
+  'Background'
+];
+
+const GRAVITY_VALUES = ['Northwest',
+  'Northeast',
+  'North',
+  'Southwest',
+  'Southeast',
+  'South',
+  'West',
+  'East'
+];
