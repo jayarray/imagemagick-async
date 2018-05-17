fc0c92052381b221fe93461ec73bf686651dbe06
Jesus Arevalo <isa90201@gmail.com>
2018-05-17 13:48:39 -0700

    Added more layer types.

--- layer.js
+++ layer.js
@@ -2,6 +2,7 @@ let CANVAS = require('./canvas.js');
 let PRIMITIVES = require('./primitives.js');
 
 //------------------------------------------
+// CANVAS
 
 /**
  * Create a color canvas layer.
@@ -35,6 +36,65 @@ function FromPath(path) {
 }
 
 /**
+ * Create a label canvas layer.
+ * @param {number} width 
+ * @param {number} height 
+ * @param {string} text 
+ * @param {string} font 
+ * @param {number} strokeWidth 
+ * @param {string} strokeColor 
+ * @param {string} fillColor 
+ * @param {string} underColor 
+ * @param {string} backgroundColor 
+ * @param {string} gravity
+ * @returns {Layer} Returns a Layer object.
+ */
+function FromLabel(width, height, text, font, strokeWidth, strokeColor, fillColor, underColor, backgroundColor, gravity) {
+  return CANVAS.CreateLabelCanvas(width, height, text, font, strokeWidth, strokeColor, fillColor, underColor, backgroundColor, gravity);
+}
+
+//--------------------------------------
+// PRIMITIVES
+
+/**
+ * Create a bezier curve.
+ * @param {Array<Coordinates>} points 
+ * @param {string} strokeColor 
+ * @param {number} strokeWidth 
+ * @param {string} fillColor 
+ */
+function Bezier(points, strokeColor, strokeWidth, fillColor) {
+  return PRIMITIVES.CreateBezier(points, strokeColor, strokeWidth, fillColor);
+}
+
+/**
+ * Create a circle.
+ * @param {Coordinates} center 
+ * @param {Coordinates} edge 
+ * @param {string} strokeColor 
+ * @param {number} strokeWidth 
+ * @param {string} fillColor 
+ */
+function Circle(center, edge, strokeColor, strokeWidth, fillColor) {
+  return PRIMITIVES.CreateCircle(center, edge, strokeColor, strokeWidth, fillColor);
+}
+
+/**
+ * Create an ellipse.
+ * @param {Coordinates} center 
+ * @param {number} width 
+ * @param {number} height 
+ * @param {string} strokeColor 
+ * @param {number} strokeWidth 
+ * @param {string} fillColor 
+ * @param {number} angleStart 
+ * @param {number} angleEnd 
+ */
+function Ellipse(center, width, height, strokeColor, strokeWidth, fillColor, angleStart, angleEnd) {
+  return PRIMITIVES.CreateEllipse(center, width, height, strokeColor, strokeWidth, fillColor, angleStart, angleEnd);
+}
+
+/**
  * Create a line.
  * @param {Coordinates} start 
  * @param {Coordinates} end 
@@ -46,11 +106,60 @@ function Line(start, end, color, width) {
   return PRIMITIVES.CreateLine(start, end, color, width);
 }
 
+/**
+ * Create a path.
+ * @param {Array<Coordinates>} points 
+ * @param {string} strokeColor 
+ * @param {number} strokeWidth 
+ * @param {string} fillColor 
+ * @param {boolean} isClosed 
+ */
+function Path(points, strokeColor, strokeWidth, fillColor, isClosed) {
+  return PRIMITIVES.CreatePath(points, strokeColor, strokeWidth, fillColor, isClosed);
+}
+
+/**
+ * Create a point.
+ * @param {number} x 
+ * @param {number} y 
+ * @param {string} color 
+ */
+function Point(x, y, color) {
+  return PRIMITIVES.CreatePoint(x, y, color);
+}
+
+/**
+ * Create text.
+ * @param {string} string 
+ * @param {string} font 
+ * @param {number} pointSize 
+ * @param {string} gravity 
+ * @param {string} strokeColor 
+ * @param {number} strokeWidth 
+ * @param {string} fillColor 
+ */
+function Text(string, font, pointSize, gravity, strokeColor, strokeWidth, fillColor) {
+  return PRIMITIVES.CreatePoint(string, font, pointSize, gravity, strokeColor, strokeWidth, fillColor);
+}
+
+//--------------------------------
+// MODS
+
+
+
+
 //-------------------------------
 // EXPORTS
 
 exports.FromColor = FromColor;
 exports.FromGradient = FromGradient;
 exports.FromPath = FromPath;
+exports.FromLabel = FromLabel;
 
+exports.Bezier = Bezier;
+exports.Circle = Circle;
+exports.Ellipse = Ellipse;
 exports.Line = Line;
+exports.Path = Path;
+exports.Point = Point;
+exports.Text = Text;
