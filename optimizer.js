56dc9030118c74a99fce3c8a9d89d3df8f2ef996
Jesus Arevalo <isa90201@gmail.com>
2018-05-17 14:26:14 -0700

    Added getter functions for Node class. Updated code to use newly introduced getter functions.

--- optimizer.js
+++ optimizer.js
@@ -1,7 +1,16 @@
 class Node {
   constructor(layer) {
     this.layer_ = layer;
+    this.parent_ = null;
     this.children_ = [];
+  }
+
+  SetParent(p) {
+    this.parent_ = p;
+  }
+
+  Parent() {
+    return this.parent_;
   }
 
   AddChild(c) {
@@ -13,11 +22,19 @@ class Node {
   }
 
   Layer() {
-    return this.layer_;
+    return this.layer_.layer;
+  }
+
+  Xoffset() {
+    return this.layer_.x;
+  }
+
+  Yoffset() {
+    return this.layer_.y;
   }
 
   Type() {
-    return this.layer_.layer.Type();
+    return this.Layer().Type();
   }
 }
 
@@ -37,7 +54,8 @@ function GetLayerHierarchy(layer) {
     flatList.push(currNode);
 
     // Add children to parent
-    let children = currNode.Layer().layer.layers_.map(l => new Node(l));
+    let children = currNode.Layer().layers_.map(l => new Node(l));
+    children.forEach(c => c.SetParent(currNode));
     children.forEach(c => currNode.AddChild(c));
 
     // Push children to stack
@@ -46,6 +64,59 @@ function GetLayerHierarchy(layer) {
 
   return { nodes: nodes.reverse(), flatlist: flatList };
 }
+
+//------------------------------
+
+function GroupByParent(flatlist) {
+  let currGroup = [];
+  let groups = [];
+
+  let prevParent = null;
+
+  for (let i = 0; flatlist.length; ++i) {
+    let currNode = flatlist[0];
+
+    if (prevParent) {
+      if (prevParent == currNode.Parent()) {
+        currGroup.push(currNode);
+      }
+      else {
+        groups.push(currGroup);
+        currGroup = [];
+        currGroup.push(currNode);
+        prevParent = currNode.Parent();
+      }
+    }
+    else {
+      prevParent = currNode.Parent();
+      currGroup.push(currNode);
+    }
+
+    if (i == flatlist.length - 1 && currGroup.length > 0) {
+      groups.push(currGroup);
+    }
+  }
+
+  return groups;
+}
+
+//---------------------------------
+
+function GroupBy
+
+//---------------------------------
+
+function IsOptimizable(flatlist) {
+
+}
+
+
+function GroupIntoOptimizedLayers(flatList) {
+  let groups = [];
+
+  // TO DO
+}
+
 
 //----------------------------
 
@@ -58,10 +129,10 @@ function HierarchyToString(nodes, indent) {
   while (stack && stack.length > 0) {
     // Get next node
     let currNode = stack.pop();
-    string += `${'  '.repeat(indent)}${currNode.Layer().layer.Type()}\n`;
+    string += `${'  '.repeat(indent)}${currNode.Layer().Type()}\n`;
 
     // Add children to parent
-    let children = currNode.Layer().layer.layers_.map(l => new Node(l));
+    let children = currNode.Layer().layers_.map(l => new Node(l));
     string += HierarchyToString(children, indent + 1);
   }
 
