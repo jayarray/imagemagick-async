c80ac9847f13113855e52bf8df3340bf618db8a5
Jesus Arevalo <isa90201@gmail.com>
2018-05-17 18:03:45 -0700

    Refactored cut module to reflect changes made to layer base class.

--- cut.js
+++ cut.js
@@ -3,130 +3,104 @@
 let VALIDATE = require('./validate.js');
 let LOCAL_COMMAND = require('linux-commands-async').Command.LOCAL;
 
+let Layer = require('./layerbase.js').Layer;
+
+//------------------------------------
+// CUT (base class)
+
+class Cut extends Layer {
+  constructor() {
+    super();
+  }
+
+  /**
+   * @returns {Array<string|number>} Returns an array of arguments.
+   */
+  Args() {
+    // Override
+  }
+
+  /**
+   * @override
+   * @returns {string} Returns a string of the command used to render the mod.
+   */
+  Command() {
+    return 'convert';
+  }
+
+  /**
+   * @override
+   * @returns {string} Returns a string of the type name.
+   */
+  Type() {
+    return 'mod';
+  }
+}
 
 //--------------------------------------
 // OUT
 
-/**
- * Cut an image out from the other. (It's like removing dough with a cookie cutter.)
- * @param {string} baseImagePath The path for the image you want to cut out of.
- * @param {string} cutoutImagePath The path for the image you want to use as a mask.
- * @param {string} outputPath The path where the resulting image will render to.
- * @returns {Promise} Returns a Promise that resolves if successful. Otherwise, it returns an error.
- */
-function Out(baseImagePath, cutoutImagePath, outputPath) {
-  let error = VALIDATE.IsStringInput(baseImagePath);
-  if (error)
-    return Promise.reject(`Failed to cut out image: base image path is ${error}`);
+class CutOut extends Cut {
+  constructor(baseImagePath, cutoutImagePath) {
+    super();
+    this.baseImagePath_ = baseImagePath;
+    this.cutoutImagePath_ = cutoutImagePath;
+  }
 
-  error = VALIDATE.IsStringInput(cutoutImagePath);
-  if (error)
-    return Promise.reject(`Failed to cut out image: cutout image path is ${error}`);
+  /**
+   * @returns {Array<string|number>} Returns an array of arguments.
+   */
+  Args() {
+    return [baseImagePath, cutoutImagePath, '-compose', 'Dst_Out', '-composite'];
+  }
 
-  error = VALIDATE.IsStringInput(outputPath);
-  if (error)
-    return Promise.reject(`Failed to cut out image: output path is ${error}`);
+  /**
+   * Create a CutOut object. Cut an image out from the other. (It's like removing dough with a cookie cutter.)
+   * @param {string} baseImagePath The path for the image you want to cut out of.
+   * @param {string} cutoutImagePath The path for the image you want to use as a mask.
+   * @returns {CutOut} Returns a CutOut object. If inputs are invalid, it returns null.
+   */
+  static Create(filepaths, gravity) {
+    if (!filepaths)
+      return null;
 
-  return new Promise((resolve, reject) => {
-    let args = [
-      baseImagePath, cutoutImagePath,
-      '-compose', 'Dst_Out',
-      '-composite', outputPath
-    ];
-
-    LOCAL_COMMAND.Execute('convert', args).then(output => {
-      if (output.stderr) {
-        reject(`Failed to cut out image: ${output.stderr}`);
-        return;
-      }
-      resolve();
-    }).catch(error => `Failed to cut out image: ${error}`);
-  });
+    return new CutOut(baseImagePath, cutoutImagePath);
+  }
 }
 
 //--------------------------------------
 // IN
 
-/**
- * Cut into an image. (It's like removing all the dough around the cookie cutter.)
- * @param {string} baseImagePath The path for the image you want to cut out of.
- * @param {string} cutoutImagePath The path for the image you want to use as a mask.
- * @param {string} outputPath The path where the resulting image will render to.
- * @returns {Promise} Returns a Promise that resolves if successful. Otherwise, it returns an error.
- */
-function In(baseImagePath, cutoutImagePath, outputPath) {
-  let error = VALIDATE.IsStringInput(baseImagePath);
-  if (error)
-    return Promise.reject(`Failed to cut into image: base image path is ${error}`);
+class CutIn extends Cut {
+  constructor(baseImagePath, cutoutImagePath) {
+    super();
+    this.baseImagePath_ = baseImagePath;
+    this.cutoutImagePath_ = cutoutImagePath;
+  }
 
-  error = VALIDATE.IsStringInput(cutoutImagePath);
-  if (error)
-    return Promise.reject(`Failed to cut into image: cutout image path is ${error}`);
+  /**
+   * @returns {Array<string|number>} Returns an array of arguments.
+   */
+  Args() {
+    return [baseImagePath, cutoutImagePath, '-compose', 'Dst_In', '-composite'];
+  }
 
-  error = VALIDATE.IsStringInput(outputPath);
-  if (error)
-    return Promise.reject(`Failed to cut into image: output path is ${error}`);
+  /**
+   * Create a CutIn object. Cut into an image. (It's like removing all the dough around the cookie cutter.)
+   * @param {string} baseImagePath The path for the image you want to cut out of.
+   * @param {string} cutoutImagePath The path for the image you want to use as a mask.
+   * @returns {CutIn} Returns a CutIn object. If inputs are invalid, it returns null.
+   */
+  static Create(filepaths, gravity) {
+    if (!filepaths)
+      return null;
 
-  return new Promise((resolve, reject) => {
-    let args = [
-      baseImagePath, cutoutImagePath,
-      '-compose', 'Dst_In',
-      '-composite', outputPath
-    ];
-
-    LOCAL_COMMAND.Execute('convert', args).then(output => {
-      if (output.stderr) {
-        reject(`Failed to cut into image: ${output.stderr}`);
-        return;
-      }
-      resolve();
-    }).catch(error => `Failed to cut into image: ${error}`);
-  });
-}
-
-//-------------------------------------
-// XOR
-
-/**
- * Cut one image out from the other.
- * @param {string} baseImagePath The path for the image you want to cut out of.
- * @param {string} cutoutImagePath The path for the image you want to use as a mask.
- * @param {string} outputPath The path where the resulting image will render to.
- * @returns {Promise} Returns a Promise that resolves if successful. Otherwise, it returns an error.
- */
-function Xor(baseImagePath, cutoutImagePath, outputPath) {
-  let error = VALIDATE.IsStringInput(baseImagePath);
-  if (error)
-    return Promise.reject(`Failed to cut out image: base image path is ${error}`);
-
-  error = VALIDATE.IsStringInput(cutoutImagePath);
-  if (error)
-    return Promise.reject(`Failed to cut out image: cutout image path is ${error}`);
-
-  error = VALIDATE.IsStringInput(outputPath);
-  if (error)
-    return Promise.reject(`Failed to cut out image: output path is ${error}`);
-
-  return new Promise((resolve, reject) => {
-    let args = [
-      baseImagePath, cutoutImagePath,
-      '-compose', 'Xor',
-      '-composite', outputPath
-    ];
-
-    LOCAL_COMMAND.Execute('convert', args).then(output => {
-      if (output.stderr) {
-        reject(`Failed to cut out image: ${output.stderr}`);
-        return;
-      }
-      resolve();
-    }).catch(error => `Failed to cut out image: ${error}`);
-  });
+    return new CutOut(baseImagePath, cutoutImagePath);
+  }
 }
 
 //------------------------------------
 // EXPORTS
 
-exports.In = In;
-exports.Out = Out;
-exports.Xor = Xor;
+exports.CreateCutOutMod = CutOut.Create;
+exports.CreateCutInMod = CutIn.Create;
