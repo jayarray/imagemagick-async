138f482c816bf08fd39376679409c0f80b421054
Jesus Arevalo <isa90201@gmail.com>
2018-05-29 14:32:28 -0700

    Added function to return number of input sources used for this layer.

--- comparebaseclass.js
+++ comparebaseclass.js
@@ -16,6 +16,13 @@ class CompareBaseClass extends LayerBaseClass {
   }
 
   /**
+   * @returns {number} Returns the number of source inputs.
+   */
+  NumberOfSources() {
+    return 2;
+  }
+
+  /**
    * @override
    * @returns {string} Returns a string of the type name.
    */
