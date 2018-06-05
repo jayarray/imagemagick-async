d8134e8137fe6cf75f44658b716c69abd375baf4
Jesus Arevalo <isa90201@gmail.com>
2018-06-05 16:38:32 -0700

    Added RotateImage mod to transform module.

--- transform.js
+++ transform.js
@@ -368,6 +368,48 @@ class RotateAroundPoint extends TransformBaseClass {
   }
 }
 
+class RotateImage extends TransformBaseClass {
+  constructor(src, degrees) {
+    super();
+    this.src_ = src;
+    this.degrees_ = degrees;
+  }
+
+  /**
+   * @returns {Array<string|number>} Returns an array of image magick arguments associated with this layer.
+   */
+  Args() {
+    return [];
+  }
+
+  /**
+   * @returns {Array<string|number>} Returns an array of arguments used for rendering this layer.
+   */
+  RenderArgs() {
+    return [];
+  }
+
+  /**
+   * @override
+   */
+  Name() {
+    return 'RotateImage';
+  }
+
+  /**
+   * Create a RotateAroundCenter object. Rotate an image around the center.
+   * @param {string} src
+   * @param {numbers} degrees Integer value representing the number of degrees to rotate the image. A positive value indicates clockwise rotation. A negative value indicates counter-clockwise rotation.
+   * @returns {RotateAroundCenter} Returns a RotateAroundCenter object. If inputs are invalid, it returns null.
+   */
+  static Create(src, degrees) {
+    if (!src || isNaN(degrees))
+      return null;
+
+    return new RotateAroundCenter(src, degrees);
+  }
+}
+
 //--------------------------------------
 // RESIZE 
 
@@ -717,4 +759,5 @@ exports.CreateResizeFillGivenAreaMod = ResizeFillGivenArea.Create;
 exports.CreateResizePercentageMod = ResizePercentage.Create;
 exports.CreateResizePixelCountLimitMod = ResizePixelCountLimit.Create;
 exports.CreateCropMod = Crop.Create;
+exports.CreateRotateImageMod = RotateImage.Create;
 
