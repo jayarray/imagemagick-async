7b8a4c17f3fc8a3659b303b68bf4e2ccda5dba5d
Jesus Arevalo <isa90201@gmail.com>
2018-06-07 15:37:57 -0700

    Changed name label test to render all fonts.

--- isasquared_name_label.js
+++ isasquared_name_label.js
@@ -7,6 +7,7 @@ let LINUX_COMMANDS = require('linux-commands-async');
 //----------------------------------
 
 let outputDir = '/home/isa/Downloads';
+let fontDir = PATH.join(outputDir, 'Font Samples');
 let format = 'png';
 
 // Create FX
@@ -15,32 +16,54 @@ let sigma = 10;
 let hasTransparency = true;
 let blur = API.Blur('?', radius, sigma, hasTransparency);
 
-// Create main canvas
-let canvasWidth = 1080;
-let canvasHeight = 1080;
-let canvas = API.ColorCanvas(canvasHeight, canvasWidth, 'black');
 
-// Create label
-let labelWidth = null;
-let labelHeight = null;
-let labelText = 'isa';
-let labelFont = 'Candice';
-let labelFontSize = 72;
-let labelStrokeWidth = 1;
-let labelStrokeColor = '#00ff55';
-let labelFillColor = null;
-let labelUnderColor = null;
-let labelBackgroundColor = null;
-let labelGravity = 'Center';
-let label = API.LabelCanvas(labelWidth, labelHeight, labelText, labelFont, labelFontSize, labelStrokeWidth, labelStrokeColor, labelFillColor, labelUnderColor, labelBackgroundColor, labelGravity);
-//label.ApplyFxOrMod(blur);
+// Get all fonts
+console.log('\nListing available fonts...');
 
+API.Fonts().then(fonts => {
+  console.log(`Found ${fonts.length} fonts!`);
+  console.log(`\nPreparing fonts...`);
 
-// RENDER
-let outputPath = PATH.join(outputDir, 'name_logo.png');
+  let actions = [];
 
-label.Render(outputPath).then(success => {
-  console.log('Success :-)');
+  let excludedFonts = ['Lohit'];
+  fonts.filter(x => x.name == 'aakar').forEach(font => {
+    // Create main canvas
+    let canvasWidth = 1080;
+    let canvasHeight = 1080;
+    let canvas = API.ColorCanvas(canvasHeight, canvasWidth, 'none');
+
+    // Create label
+    let labelWidth = canvasWidth;
+    let labelHeight = canvasHeight;
+    let labelText = 'ISA';
+    let labelFont = font.name;
+    let labelFontSize = 200;
+    let labelKerning = 20;
+    let labelStrokeWidth = 1;
+    let labelStrokeColor = '#ffffff';
+    let labelFillColor = labelStrokeColor;
+    let labelUnderColor = null;
+    let labelBackgroundColor = 'none';
+    let labelGravity = 'Center';
+    let label = API.LabelCanvas(labelWidth, labelHeight, labelText, labelFont, labelFontSize, labelKerning, labelStrokeWidth, labelStrokeColor, labelFillColor, labelUnderColor, labelBackgroundColor, labelGravity);
+
+    canvas.Draw(label, 0, 0);
+
+    let outputPath = PATH.join(fontDir, `${labelFont}.${format}`);
+    actions.push(canvas.Render(outputPath));
+  });
+
+  // Render all
+  console.log(`Done!`);
+  console.log('\nRendering all fonts...');
+
+  Promise.all(actions).then(success => {
+    console.log(`Done!`);
+    console.log('\nSuccess :-)');
+  }).catch(error => {
+    console.log(`ERROR rendering fonts: ${error}`);
+  });
 }).catch(error => {
-  console.log(`ERROR: ${error}`);
+  console.log(`ERROR listing fonts: ${error}`);
 });
