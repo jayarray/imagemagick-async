023f44d5c0b6ea4934e32bbfc194609f663c658e
Jesus Arevalo <isa90201@gmail.com>
2018-06-11 21:14:51 -0700

    Created script test module.

--- script_test.js
+++ script_test.js
@@ -6,3 +6,9 @@ let PATH = require('path');
 let dir = '/home/isa/Downloads';
 
 let filepath = PATH.join(dir, 'drawtest.txt');
+
+SCRIPT.Execute(filepath).then(success => {
+  console.log('Success :-)');
+}).catch(error => {
+  console.log(`ERROR: ${error}`);
+});
