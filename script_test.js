1c28d3e8eddb4aebf9dfdbd33d8777c8fe477607
Jesus Arevalo <isa90201@gmail.com>
2018-06-11 18:39:12 -0700

    Moved script test module back into original folder.

---
+++ script_test.js
@@ -0,0 +1,8 @@
+let SCRIPT = require('./script.js');
+let PATH = require('path');
+
+//----------------------------
+
+let dir = '/home/isa/Downloads';
+
+let filepath = PATH.join(dir, 'drawtest.txt');
