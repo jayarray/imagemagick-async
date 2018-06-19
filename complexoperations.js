4977f5393e2e77b89edc205e34e3575341192d24
Jesus Arevalo <isa90201@gmail.com>
2018-06-18 22:01:09 -0700

    Added import for primitives class and added ReflectImage function.

--- complexoperations.js
+++ complexoperations.js
@@ -1,7 +1,9 @@
 let IDENTIFY = require('./identify.js');
 let LOCAL_COMMAND = require('linux-commands-async').Command.LOCAL;
+let PRIMITIVES = require('./primitives.js');
 
 //--------------------------------
+// ROTATE IMAGE
 
 /**
  * Rotate image.
@@ -39,7 +41,66 @@ function RotateImage(src, degrees, consolidatedEffects, outputPath) {
   });
 }
 
+//------------------------------------
+// REFLECT
+
+/**
+ * Reflect image over a line.
+ * @param {string} src 
+ * @param {number} degrees 
+ * @param {string} outputPath 
+ * @returns {Promise<{xOffset: number, yOffset: number}>}
+ */
+function ReflectImage(src, x0, y0, x1, y1, consolidatedEffects, outputPath) {
+  return new Promise((resolve, reject) => {
+    IDENTIFY.ImageInfo(src).then(obj => {
+      // Get all non-transparent pixel infos
+      let pixelObjs = obj.filter(p => !p.isTransparent).map(p => new PRIMITIVES.CreatePoint(p.x, p.y, p.color));
+
+      // Compute line slope
+      let dy = y1 - y0;
+      let dx = x1 - x0;
+      let m = dy / dx;
+
+      // Change pixel coordinates to reflected coordinates
+      for (let i = 0; i < pixelObjs.length; ++i) {
+        let currPixel = pixelObjs[i];
+
+        // Compute new coordinates (Matrix algebra)
+        let topLeft = 1 - Math.pow(m, 2);
+        let topRight = (2 * m) / (Math.pow(m, 2) + 1);
+        let slopeMatrix = [[topLeft, topRight], [topRight, -topLeft]];
+        let pointMatrix = [currPixel.x_, currPixel.y_];
+
+        let reflectedX = (slopeMatrix[0][0] * pointMatrix[0]) + (slopeMatrix[0][1] * pointMatrix[1]);
+        let reflectedY = (slopeMatrix[1][0] * pointMatrix[0]) + (slopeMatrix[1][1] * pointMatrix[1]);
+
+        // Update pixel coordinates
+        currPixel.x_ = reflectedX;
+        currPixel.y_ = reflectedY;
+      }
+
+      // Build command
+      let args = [src];
+      pixelObjs.forEach(p => args = args.concat(p.Args()));
+      consolidatedEffects.forEach(c => args = args.concat(c.Args()));
+      args.push(outputPath);
+
+      // Render
+      LOCAL_COMMAND.Execute('convert', args).then(output => {
+        if (output.stderr) {
+          reject(output.stderr);
+          return;
+        }
+
+        resolve();
+      }).catch(error => reject(error));
+    }).catch(error => reject(error));
+  });
+}
+
 //-----------------------------------
 // EXPORTS
 
 exports.RotateImage = RotateImage;
+exports.ReflectImage = ReflectImage;
