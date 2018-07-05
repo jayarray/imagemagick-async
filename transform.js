b2a6db9e4d5becc24fe650df891ccaec755411e5
Jesus Arevalo <isa90201@gmail.com>
2018-07-05 13:38:46 -0700

    Added Trim class to Transform module.

--- transform.js
+++ transform.js
@@ -793,6 +793,62 @@ class Crop extends TransformBaseClass {
   }
 }
 
+//------------------------------------
+// TRIM
+
+class Trim extends TransformBaseClass {
+  constructor(src, borderColor, fuzz) {
+    this.src_ = src;
+    this.borderColor_ = borderColor;
+    this.fuzz_ = fuzz;
+  }
+
+  /**
+   * @returns {Array<string|number>} Returns an array of image magick arguments associated with this layer.
+   */
+  Args() {
+    let args = [];
+
+    if (this.borderColor_)
+      args.push('-bordercolor', this.borderColor_);
+
+    if (this.fuzz_ && this.fuzz_ > 0)
+      args.push('-fuzz', this.fuzz_);
+
+    args.push('-trim', '+repage');
+
+    return args;
+  }
+
+  /**
+   * @returns {Array<string|number>} Returns an array of arguments used for rendering this layer.
+   */
+  RenderArgs() {
+    [this.src_].concat(this.Args());
+  }
+
+  /**
+   * @override
+   */
+  Name() {
+    return 'Trim';
+  }
+
+  /**
+   * Create a Trim object. Trim surrounding transparent pixels from an image.
+   * @param {string} src
+   * @param {string} borderColor
+   * @param {number} fuzz
+   * @returns {Trim} Returns a Trim object.
+   */
+  static Create(src, borderColor, fuzz) {
+    if (!src)
+      return null;
+
+    return new Trim(src, borderColor, fuzz);
+  }
+}
+
 //-----------------------------------
 // EXPORTS
 
@@ -813,4 +869,4 @@ exports.CreateResizePercentageMod = ResizePercentage.Create;
 exports.CreateResizePixelCountLimitMod = ResizePixelCountLimit.Create;
 exports.CreateCropMod = Crop.Create;
 exports.CreateRotateImageMod = RotateImage.Create;
-
+exports.CreateTrimMod = Trim.Create;
