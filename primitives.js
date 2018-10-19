62ff6fb3d499f8be9ce14b0d599a6d9b6747f9d4
Jesus Arevalo <isa90201@gmail.com>
2018-10-18 17:14:15 -0700

    Added PathComposite for creating a path made up different line segments.

--- primitives.js
+++ primitives.js
@@ -1,4 +1,5 @@
 let COORDINATES = require('./coordinates.js');
+let LINE_SEGMENTS = require('./linesegments.js');
 let PrimitiveBaseClass = require('./primitivesbaseclass.js').PrimitiveBaseClass;
 
 //--------------------------------
@@ -334,6 +335,82 @@ class Path extends PrimitiveBaseClass {
 }
 
 //--------------------------------
+// PATH COMPOSITE
+
+class PathComposite extends PrimitiveBaseClass {
+  /** 
+  * @param {Coordinates} start The start coordinates of your path. (Required)
+  * @param {Array<>} lineSegments A list of LineSegment objects to be connected in the order provided. (Required)
+  * @param {string} strokeColor The color of the line connecting all the points. (Valid color format string used in Image Magick) (Optional)
+  * @param {number} strokeWidth Width of the line connecting all the points. (Larger values produce thicker lines.) (Optional)
+  * @param {string} fillColor The color to fill the path with. (Valid color format string used in Image Magick) (Optional)
+  * @param {boolean} isClosed Set to true if you wish to connect the last point back to the first one (if not done already) with a straight line. Else, set to false.
+  */
+  constructor(start, lineSegments, strokeColor, strokeWidth, fillColor, isClosed) {
+    super();
+    this.start_ = start;
+    this.lineSegments_ = lineSegments;
+    this.strokeColor_ = strokeColor;
+    this.strokeWidth_ = strokeWidth;
+    this.fillColor_ = fillColor;
+    this.isClosed_ = isClosed;
+  }
+
+  /** 
+   * Get a list of points in string form that have the X and Y offsets applied to them.
+   * @returns {string} Returns a space-delimited string representing all points in the path.
+   */
+  LineSegmentsToString() {
+    return this.lineSegments_.map(l => l.String()).join(' ');
+  }
+
+  /** 
+   * @override
+   * @returns {Array<string|number>} Returns an array of arguments needed for drawing the path.
+   */
+  Args() {
+    let args = [];
+
+    if (this.fillColor_)
+      args.push('-fill', this.fillColor_);
+    else
+      args.push('-fill', 'none'); // Prevents default black fill color
+
+    if (this.strokeColor_)
+      args.push('-stroke', this.strokeColor_);
+
+    if (this.strokeWidth_)
+      args.push('-strokewidth', this.strokeWidth_);
+
+    let drawString = `path 'M ${this.start_.x_},${this.start_.y_} ${this.LineSegmentsToString()}`;
+
+    if (this.isClosed_)
+      drawString += ' Z';
+    drawString += `'`;
+
+    args.push('-draw', drawString);
+    return args;
+  }
+
+  /** 
+   * Create a Path object with the specified properties.
+   * @param {Coordinates} start The start coordinates of your path. (Required)
+   * @param {Array<>} lineSegments A list of LineSegment objects to be connected in the order provided. (Required)
+   * @param {string} strokeColor The color of the line connecting all the points. (Valid color format string used in Image Magick) (Optional)
+   * @param {number} strokeWidth Width of the line connecting all the points. (Larger values produce thicker lines.) (Optional)
+   * @param {string} fillColor The color to fill the path with. (Valid color format string used in Image Magick) (Optional)
+   * @param {boolean} isClosed Set to true if you wish to connect the last point back to the first one (if not done already) with a straight line. Else, set to false.
+   * @returns {PathComposite} Returns a Path object. If inputs are invalid, it returns null.
+   */
+  static Create(start, lineSegments, strokeColor, strokeWidth, fillColor, isClosed) {
+    if (!start || !lineSegments || lineSegments.length < 1)
+      return null;
+
+    return new PathComposite(start, lineSegments, strokeColor, strokeWidth, fillColor, isClosed);
+  }
+}
+
+//--------------------------------
 // POINT
 
 class Point extends PrimitiveBaseClass {
@@ -458,5 +535,6 @@ exports.CreateCircle = Circle.Create;
 exports.CreateEllipse = Ellipse.Create;
 exports.CreateLine = Line.Create;
 exports.CreatePath = Path.Create;
+exports.CreatePathComposite = PathComposite.Create;
 exports.CreatePoint = Point.Create;
 exports.CreateText = Text.Create;
