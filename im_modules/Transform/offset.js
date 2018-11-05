a675a4c3d536a6f06a6b14ce9b8c1b0df605f2b7
Jesus Arevalo <isa90201@gmail.com>
2018-11-05 11:21:58 -0800

    Consolidate x and y values into a coordinates object.

--- im_modules/Transform/offset.js
+++ im_modules/Transform/offset.js
@@ -4,20 +4,18 @@ let TRANSFORM_BASECLASS = require(PATH.join(__dirname, 'transformbaseclass.js'))
 //-----------------------------------
 
 class Offset extends TRANSFORM_BASECLASS {
-  constructor(src, x0, y0, x1, y1) {
+  constructor(src, start, end) {
     super();
     this.src_ = src;
-    this.x0_ = x0;
-    this.y0_ = y0;
-    this.x1_ = x1;
-    this.y1_ = y1;
+    this.start_ = start;
+    this.end_ = end;
   }
 
   /**
    * @returns {Array<string|number>} Returns an array of image magick arguments associated with this layer.
    */
   Args() {
-    return ['-virtual-pixel', 'transparent', '-distort', 'Affine', `${this.x0_},${this.y0_} ${this.x1_},${this.y1_}`];
+    return ['-virtual-pixel', 'transparent', '-distort', 'Affine', `${this.start_.x_},${this.start_.y_} ${this.end_.x_},${this.end_.y_}`];
   }
 
   /**
@@ -37,17 +35,15 @@ class Offset extends TRANSFORM_BASECLASS {
   /**
    * Create an Offset object.Shift an image relative to the start and end coordinates. Shift is computed as: Xshift = x1 - x0 and Yshift = y1 - y0.
    * @param {string} src
-   * @param {number} x0 Start X-coordinate
-   * @param {number} y0 Start Y-coordinate
-   * @param {number} x1 End X-coordinate
-   * @param {number} y1 End Y-coordinate
+   * @param {Coordinates} start Relative start position.
+   * @param {Coordinates} end Relative end position.
    * @returns {Offset} Returns an Offset object. If inputs are invalid, it returns null.
    */
-  static Create(src, x0, y0, x1, y1) {
-    if (!src || isNaN(x0) || isNaN(y0) || isNaN(x1) || isNaN(y1))
+  static Create(src, start, end) {
+    if (!src || !start || !end)
       return null;
 
-    return new Offset(src, x0, y0, x1, y1);
+    return new Offset(src, start, end);
   }
 }
 
