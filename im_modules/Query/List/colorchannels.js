e792e2e77cac75bfeb31befc023d7826f5a2cc4f
Jesus Arevalo <isa90201@gmail.com>
2018-11-29 17:38:36 -0800

    Moved color channels module to List folder.

--- im_modules/Query/List/colorchannels.js
+++ im_modules/Query/List/colorchannels.js
@@ -5,7 +5,7 @@ let LOCAL_COMMAND = require('linux-commands-async').Command.LOCAL;
 /**
  * @returns {Promise<Array<string>>} Returns a Promise. If it resolves, it returns a list of color channel names. Otherwise, it returns an error.
  */
-function Channels() {
+function ColorChannels() {
   return new Promise((resolve, reject) => {
     let args = ['-list', 'channel']; // convert:  prints newline delimited list of channels
 
@@ -24,8 +24,8 @@ function Channels() {
 //------------------------
 // EXPORTS
 
-exports.Channels = Channels;
+exports.ColorChannels = ColorChannels;
 
-exports.Name = 'Channels';
+exports.Name = 'ColorChannels';
 exports.ComponentType = 'function';
-exports.Func = Channels;
+exports.Func = ColorChannels;
