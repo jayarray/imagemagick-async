d795ad09bfd1d1a7c3b98640679a3f6d9cdf069b
Jesus Arevalo <isa90201@gmail.com>
2019-02-11 13:43:53 -0800

    Updated animation baseclass to reflect object interface implementation.

--- im_modules/Animation/animationbaseclass.js
+++ im_modules/Animation/animationbaseclass.js
@@ -3,31 +3,23 @@ let parts = __dirname.split(PATH.sep);
 let index = parts.findIndex(x => x == 'im_modules');
 let IM_MODULES_DIR = parts.slice(0, index + 1).join(PATH.sep);
 let OBJECT_INTERFACE = require(PATH.join(__dirname, 'objectinterface.js')).ObjectInterface;
-let CHECKS = require(PATH.join(IM_MODULES_DIR, 'Checks', 'check.js'));
 
 //--------------------------------------
 
 class AnimationBaseClass extends OBJECT_INTERFACE {
   constructor(properties) {
     super({ category: 'animation' });
+    this.type = properties.type;
+    this.name = properties.name;
+    this.args = properties.args;
+    this.command = properties.command;
   }
 
   /**
-   * @override
+   * @returns {Array} Returns a list of arguments needed for rendering.
    */
   Args() {
-  }
-
-  /**
-   * @override
-   */
-  Params() {
-  }
-
-  /**
-   * @override
-   */
-  Errors() {
+    // override
   }
 }
 
