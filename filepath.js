ff3450b5104882bb203666404c67f0a0a44071ef
Jesus Arevalo <isa90201@gmail.com>
2019-02-12 12:31:29 -0800

    Added more functions to get specific directory paths.

--- filepath.js
+++ filepath.js
@@ -7,6 +7,9 @@ class Filepath {
     this.infos = builder.infos;
   }
 
+  /**
+   * @returns {string} Returns the full path.
+   */
   String() {
     let str = builder.infos.map(x => x.name).join(PATH.sep);
     return str;
@@ -63,8 +66,196 @@ class Filepath {
     return Builder;
   }
 
+  /**
+   * @returns {string} Returns the path to the project directory.
+   */
   static RootDir() {
     return __dirname;
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static AnimationDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Animation')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static ConstantsDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Constants')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static DrawablesDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Drawables')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static CanvasDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Drawables')
+      .dir('Canvas')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static PrimitivesDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Drawables')
+      .dir('Primitives')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static ShapesDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Drawables')
+      .dir('Shapes')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static EffectsDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Effects')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static FxDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Effects')
+      .dir('Fx')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static ModDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Effects')
+      .dir('Mod')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static TransformDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Effects')
+      .dir('Transform')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static InputsDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Inputs')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static GradientDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Inputs')
+      .dir('Gradient')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static LineSegmentsDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Inputs')
+      .dir('LineSegments')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static LayerDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Layer')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static QueryDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Query')
+      .build()
+      .String();
+  }
+
+  /**
+   * @returns {string} Returns a path to this directory.
+   */
+  static ListDir() {
+    return Filepath.Builder()
+      .partialPath(Filepath.RootDir)
+      .dir('Query')
+      .dir('List')
+      .build()
+      .String();
   }
 }
 
