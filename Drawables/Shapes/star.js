857e9e34f729351acf0092e5b4db92a7226ab5cd
Jesus Arevalo <isa90201@gmail.com>
2019-02-13 15:03:37 -0800

    Refactored star module to reflect changes made to primitives base class. Implements builder pattern.

--- Drawables/Shapes/star.js
+++ Drawables/Shapes/star.js
@@ -1,114 +1,294 @@
-let PATH = require('path');
-
-let parts = __dirname.split(PATH.sep);
-let index = parts.findIndex(x => x == 'im_modules');
-let IM_MODULES_DIR = parts.slice(0, index + 1).join(PATH.sep);
-let COORDINATES = require(PATH.join(IM_MODULES_DIR, 'Inputs', 'coordinates.js')).Create;
-let PATH_PRIMITIVE = require(PATH.join(IM_MODULES_DIR, 'Primitives', 'path.js')).Create;
-let PRIMITIVE_BASECLASS = require(PATH.join(IM_MODULES_DIR, 'Primitives', 'primitivesbaseclass.js')).PrimitiveBaseClass;
-let HELPER_FUNCTIONS = require(PATH.join(__dirname, 'helperfunctions.js'));
+let Path_ = require('path');
+let Validate = require('./validate.js');
+let Filepath = require('./filepath.js').Filepath;
+let Path = require(Path_.join(Filepath.PrimitivesDir(), 'path.js')).Path;
+let Coordinates = require(Path_.join(Filepath.InputsDir(), 'coordinates.js')).Coordinates;
+let HelperFunctions = require(Path_.join(Filepath.ShapesDir(), 'helperfunctions.js'));
+let PrimitivesBaseClass = require(Path_.join(Filepath.PrimitivesDir(), 'primitivesbaseclass.js')).PrimitivesBaseClass;
 
 //---------------------------------
 
-class Star extends PRIMITIVE_BASECLASS {
-  constructor(vertices, center, vertex, bloat, strokeColor, strokeWidth, fillColor) {
-    super();
-    this.vertices_ = vertices;
-    this.center_ = center;
-    this.vertex_ = vertex;
-    this.bloat_ = bloat;
-    this.strokeColor_ = strokeColor;
-    this.strokeWidth_ = strokeWidth;
-    this.fillColor_ = fillColor;
-    this.path_ = this.BuildPath_(vertices, center, vertex, bloat, strokeColor, strokeWidth, fillColor);
+class Star extends PrimitivesBaseClass {
+  constructor(properties) {
+    super(properties);
   }
 
-  BuildPath_(vertices, center, vertex, bloat, strokeColor, strokeWidth, fillColor) {
-    let majorDegrees = 360 / vertices;
+  /**
+   * @override
+   */
+  static get Builder() {
+    class Builder {
+      constructor() {
+        this.name = 'Star';
+        this.args = {};
+      }
 
+      /**
+       * @param {number} vertices Number of points on the star. 
+       */
+      vertices(vertices) {
+        this.args.vertices = vertices;
+        return this;
+      }
+
+      /**
+       * @param {Coordinates} center The center of the star.
+       */
+      center(center) {
+        this.args.center = center;
+        return this;
+      }
+
+
+      /**
+       * @param {Coordinates} vertex A point belonging to the star.
+       */
+      vertex(vertex) {
+        this.args.vertex = vertex;
+        return this;
+      }
+
+      /**
+       * @param {number} bloat Factor equal to or greater than zero that affects the thickness of the star. The higher the value, the more 'bloated' the star will look, and will eventually invert.
+       */
+      bloat(bloat) {
+        this.args.bloat = bloat;
+        return this;
+      }
+
+      /**
+       * @param {Color} strokeColor The color of the outline of the star. (Optional)
+       */
+      strokeColor(strokeColor) {
+        this.args.strokeColor = strokeColor;
+        return this;
+      }
+
+      /**
+       * @param {number} strokeWidth Width of the outline of the star. Larger values produce thicker lines. (Optional)
+       */
+      strokeWidth(strokeWidth) {
+        this.args.strokeWidth = strokeWidth;
+        return this;
+      }
+
+      /**
+       * @param {Color} fillColor The color that will fill the star. (Optional)
+       */
+      fillColor(fillColor) {
+        this.args.fillColor = fillColor;
+        return this;
+      }
+
+      /**
+       * @param {number} x 
+       * @param {number} y 
+       */
+      offset(x, y) {
+        this.offset = { x: x, y: y };
+        return this;
+      }
+
+      build() {
+        return new Star(this);
+      }
+    }
+    return Builder;
+  }
+
+  /** 
+   * @override
+   */
+  Args() {
     // Get all major vertices
-    let majorVertices = [vertex];
+    let majorDegrees = 360 / this.args.vertices;
+    let majorVertices = [this.args.vertex];
 
-    for (let i = 0; i < vertices - 1; ++i) {
-      let rotatedPoint = HELPER_FUNCTIONS.GetRotatedPoint(center, vertex, majorDegrees * (i + 1));
+    for (let i = 0; i < this.args.vertices - 1; ++i) {
+      let rotatedPoint = HelperFunctions.GetRotatedPoint(this.args.center, this.args.vertex, majorDegrees * (i + 1));
       majorVertices.push(rotatedPoint);
     }
 
     // Compute minor vertex
-    let slope = HELPER_FUNCTIONS.GetSlope(center, vertex);
-    let yIntercept = center.y_ - (center.x_ * slope);
+    let slope = HelperFunctions.GetSlope(this.args.center, this.args.vertex);
+    let yIntercept = this.args.center.args.y - (this.args.center.args.x * slope);
     let minorX = null;
     let minorY = null;
 
-    if (center.x_ == vertex.x_) { // Vertical slope
-      minorX = center.x_;
-      minorY = center.y_ < vertex.y_ ? center.y_ + bloat : center.y_ - bloat;
+    if (this.args.center.args.x == this.args.vertex.args.x) { // Vertical slope
+      minorX = this.args.center.args.x;
+      minorY = this.args.center.args.y < this.args.vertex.args.y ? this.args.center.args.y + this.args.bloat : this.args.center.args.y - this.args.bloat;
     }
     else {
-      if (center.y_ == vertex.y_) { // Horizontal slope
-        minorX = center.x_ > vertex.x_ ? center.x_ - bloat : center.x_ + bloat;
-        minorY = center.y_;
+      if (this.args.center.args.y == this.args.vertex.args.y) { // Horizontal slope
+        minorX = this.args.center.args.x > this.args.vertex.args.x ? this.args.center.args.x - this.args.bloat : this.args.center.args.x + this.args.bloat;
+        minorY = this.args.center.args.y;
       }
       else { // Diagonal slope
-        minorX = center.x_ < vertex.x_ ? center.x_ + bloat : center.x_ - bloat;
+        minorX = this.args.center.args.x < this.args.vertex.args.x ? this.args.center.args.x + this.args.bloat : this.args.center.args.x - this.args.bloat;
         minorY = (slope * minorX) + yIntercept;
       }
     }
 
     // Get all minor vertices
     let minorDegrees = majorDegrees / 2;
-    let minorVertex = COORDINATES(parseInt(minorX), parseInt(minorY));
-    let rotatedMinorVertex = HELPER_FUNCTIONS.GetRotatedPoint(center, minorVertex, minorDegrees);
+
+    let minorVertex = Coordinates.Builder()
+      .x(parseInt(minorX))
+      .y(parseInt(minorY))
+      .build();
+
+    let rotatedMinorVertex = HelperFunctions.GetRotatedPoint(this.args.center, minorVertex, minorDegrees);
     let minorVertices = [rotatedMinorVertex];
 
-    for (let i = 0; i < vertices - 1; ++i) {
-      let rotatedPoint = HELPER_FUNCTIONS.GetRotatedPoint(center, rotatedMinorVertex, majorDegrees * (i + 1));
+    for (let i = 0; i < this.args.vertices - 1; ++i) {
+      let rotatedPoint = HelperFunctions.GetRotatedPoint(this.args.center, rotatedMinorVertex, majorDegrees * (i + 1));
       minorVertices.push(rotatedPoint);
     }
 
     // Connect major and minor vertices in alternating order
     let combinedVertices = [];
-    for (let i = 0; i < vertices; ++i) {
+    for (let i = 0; i < this.args.vertices; ++i) {
       combinedVertices.push(majorVertices[i], minorVertices[i]);
     }
 
-    // Return path
-    return PATH_PRIMITIVE(combinedVertices, strokeColor, strokeWidth, fillColor, true);
+    // Build path
+    let path = Path.Builder()
+      .points(combinedVertices)
+      .strokeColor(this.args.strokeColor)
+      .strokeWidth(this.args.strokeWidth)
+      .fillColor(this.args.fillColor)
+      .isClosed(true)
+      .build();
+
+
+    return path.Args();
   }
 
-  /** 
+  /**
    * @override
-   * @returns {Array<string|number>} Returns an array of arguments needed for drawing the polygon.
    */
-  Args() {
-    return this.path_.Args();
+  Errors() {
+    let params = Star.Parameters();
+    let errors = [];
+
+    // Check required args
+
+    if (!Validate.IsDefined(this.args.vertices))
+      errors.push('STAR_SHAPE_ERROR: Vertices is undefined.');
+    else {
+      if (!Validate.IsInteger(this.args.vertices))
+        errors.push('STAR_SHAPE_ERROR: Vertices is not an integer.');
+      else {
+        if (this.args.vertices < params.vertices.min)
+          errors.push(`STAR_SHAPE_ERROR: Vertices is out of bounds. Assigned value is: ${this.args.vertices}. Value must be greater than or equal to ${params.vertices.min}.`);
+      }
+    }
+
+    if (!Validate.IsDefined(this.args.center))
+      errors.push('STAR_SHAPE_ERROR: Center is undefined.');
+    else {
+      if (this.args.center.type != 'Coordinates')
+        errors.push('STAR_SHAPE_ERROR: Center is not a Coordinates object.');
+      else {
+        let errs = this.args.center.Errors();
+        if (errs.length > 0)
+          errors.push(`STAR_SHAPE_ERROR: Center has errors: ${errs.join(' ')}`);
+      }
+    }
+
+    if (!Validate.IsDefined(this.args.vertex))
+      errors.push('STAR_SHAPE_ERROR: Vertex is undefined.');
+    else {
+      if (this.args.vertex.type != 'Coordinates')
+        errors.push('STAR_SHAPE_ERROR: Vertex is not a Coordinates object.');
+      else {
+        let errs = this.args.vertex.Errors();
+        if (errs.length > 0)
+          errors.push(`STAR_SHAPE_ERROR: Vertex has errors: ${errs.join(' ')}`);
+      }
+    }
+
+    // Check optional args
+
+    if (this.args.bloat) {
+      if (!Validate.IsInteger(this.args.bloat))
+        errors.push('STAR_SHAPE_ERROR: Bloat is not an integer.');
+      else {
+        if (this.args.bloat < params.bloat.min)
+          errors.push(`STAR_SHAPE_ERROR: Bloat is out of bounds. Assigned value is: ${this.args.bloat}. Value must be greater than or equal to ${params.bloat.min}.`);
+      }
+    }
+
+    if (this.args.strokeColor) {
+      if (this.args.strokeColor.type != 'Color')
+        errors.push('STAR_SHAPE_ERROR: Stroke color is not a Color object.');
+      else {
+        let errs = this.args.strokeColor.Errors();
+        if (errs.length > 0)
+          errors.push(`STAR_SHAPE_ERROR: Stroke color has errors: ${errs.join(' ')}`);
+      }
+    }
+
+    if (this.args.strokeWidth) {
+      if (!Validate.IsInteger(this.args.strokeWidth))
+        errors.push('STAR_SHAPE_ERROR: Stroke width is not an integer.');
+      else {
+        if (this.args.strokeWidth < params.strokeWidth.min)
+          errors.push(`STAR_SHAPE_ERROR: Stroke width is out of bounds. Assigned value is: ${this.args.strokeWidth}. Value must be greater than or equal to ${params.strokeWidth.min}.`);
+      }
+    }
+
+    if (this.args.fillColor) {
+      if (this.args.fillColor.type != 'Color')
+        errors.push('STAR_SHAPE_ERROR: Fill color is not a Color object.');
+      else {
+        let errs = this.args.fillColor.Errors();
+        if (errs.length > 0)
+          errors.push(`STAR_SHAPE_ERROR: Fill color has errors: ${errs.join(' ')}`);
+      }
+    }
+
+    return errors;
   }
 
-  /** 
-   * Create a Polygon object with the specified properties.
-   * @param {number} vertices Number of points on the star. (Required)
-   * @param {Coordinates} center (Required)
-   * @param {Coordinates} vertex (Required)
-   * @param {number} bloat Factor equal to or greater than zero that affects the thickness of the star. The higher the value, the more 'bloated' the star will look, and will eventually invert. (Required)
-   * @param {string} strokeColor The color of the line connecting all the points. (Valid color format string used in Image Magick) (Optional)
-   * @param {number} strokeWidth Width of the line connecting all the points. (Larger values produce thicker lines.) (Optional)
-   * @param {string} fillColor The color to fill the path with. (Valid color format string used in Image Magick) (Optional)
-   * @returns {Star} Returns a Star object. If inputs are invalid, it returns null. Vertices must be equal to or greater than 4.
+  /**
+   * @override
    */
-  static Create(vertices, center, vertex, bloat, strokeColor, strokeWidth, fillColor) {
-    if (!vertices || vertices < 4 || !center || !vertex || bloat < 0)
-      return null;
-
-    return new Star(vertices, center, vertex, bloat, strokeColor, strokeWidth, fillColor);
+  static Parameters() {
+    return {
+      vertices: {
+        type: 'number',
+        subtype: 'integer',
+        min: 4
+      },
+      center: {
+        type: 'Coordinates'
+      },
+      vertex: {
+        type: 'Coordinates'
+      },
+      bloat: {
+        type: 'number',
+        subtype: 'integer',
+        min: 0
+      },
+      strokeColor: {
+        type: 'Color'
+      },
+      strokeWidth: {
+        type: 'number',
+        subtype: 'integer',
+        min: 1
+      },
+      fillColor: {
+        type: 'Color'
+      }
+    };
   }
 }
 
 //---------------------------
 // EXPORTS
 
-exports.Create = Star.Create;
-exports.Name = 'Star';
-exports.Layer = false;
-exports.Consolidate = true;
-exports.Dependencies = null;
-exports.ComponentType = 'drawable';
+exports.Star = Star;
