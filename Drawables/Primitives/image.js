594136a87e5c6078412a8d7a4576f386bc2a470f
Jesus Arevalo <isa90201@gmail.com>
2019-02-12 21:14:26 -0800

    Refactored image module to reflect changes made to primitive base class.

--- Drawables/Primitives/image.js
+++ Drawables/Primitives/image.js
@@ -1,51 +1,187 @@
-let PATH = require('path');
-
-let parts = __dirname.split(PATH.sep);
-let index = parts.findIndex(x => x == 'im_modules');
-let IM_MODULES_DIR = parts.slice(0, index + 1).join(PATH.sep);
-let PRIMITIVE_BASECLASS = require(PATH.join(__dirname, 'primitivesbaseclass.js')).PrimitiveBaseClass;
+let Path = require('path');
+let Validate = require('./validate.js');
+let Filepath = require('./filepath.js').Filepath;
+let PrimitivesBaseClass = require(Path.join(Filepath.PrimitivesDir(), 'primitivesbaseclass.js')).PrimitivesBaseClass;
 
 //----------------------------------
 
-class Image extends PRIMITIVE_BASECLASS {
-  constructor(src, corner, width, height) {
-    super();
-    this.src_ = src;
-    this.corner_ = corner;
-    this.width_ = width;
-    this.height_ = height;
+class Image extends PrimitivesBaseClass {
+  constructor(properties) {
+    super(properties);
+  }
+
+  /**
+   * @override
+   */
+  static get Builder() {
+    class Builder {
+      constructor() {
+        this.name = 'Image';
+        this.args = {};
+      }
+
+      /**
+       * @param {string} src Source path
+       */
+      src(src) {
+        this.args.src = src;
+        return this;
+      }
+
+      /**
+       * @param {Coordinates} corner The top-left corner from which the image will render.
+       */
+      corner(corner) {
+        this.args.corner = corner;
+        return this;
+      }
+
+      /**
+       * @param {number} width Desired width (if different from current width). Leave undefined if you want preserve the original width and height. (Optional)
+       */
+      width(width) {
+        this.args.width = width;
+        return this;
+      }
+
+      /**
+       * @param {number} height Desired height (if different from current height). Leave undefined if you want preserve the original width and height. (Optional)
+       */
+      height(height) {
+        this.args.height = height;
+        return this;
+      }
+
+      /**
+       * @param {number} x 
+       * @param {number} y 
+       */
+      offset(x, y) {
+        this.offset = { x: x, y: y };
+        return this;
+      }
+
+      build() {
+        return new Image(this);
+      }
+    }
+    return Builder;
   }
 
   /** 
    * @override
-   * @returns {Array<string|number>} Returns an array of arguments needed for drawing the image.
   */
   Args() {
-    return ['-draw', `image over ${this.corner_.x_},${this.corner_.y_} ${this.width_},${this.height_} ${this.src_}`];
+    let params = Image.Parameters();
+    let width = params.height.default;
+    let height = params.height.default;
+
+    let widthIsValid = Validate.IsDefined(this.args.width) && Validate.IsInteger(this.args.width) && this.args.width >= params.width.min;
+    let heightIsValid = Validate.IsDefined(this.args.height) && Validate.IsInteger(this.ars.height) && this.args.height >= params.height.min;
+
+    if (widthIsValid && heightIsValid) {
+      width = this.args.width;
+      height = this.args.height;
+    }
+
+    return ['-draw', `image over ${this.corner.String()} ${width},${height} ${this.args.src}`];
   }
 
   /**
-   * Create an Image object with the specified properties.
-   * @param {string} src
-   * @param {Coordinates} corner The top-left corner from which the image will render.
-   * @param {number} width Desired width (if different from current width). Set to zero (or null) if you want preserve the original width and height.
-   * @param {number} height Desired height (if different from current height). Set to zero (or null) if you want preserve the original width and height.
-   * @returns {Image} Returns an Image object. If inputs are invalid, it returns null.
+   * @override
    */
-  static Create(src, corner, width, height) {
-    if (!src || !corner || isNaN(width) || isNaN(height))
-      return null;
+  Errors() {
+    let params = Image.Parameters();
+    let errors = [];
 
-    return new Image(src, corner, width, height);
+    // Check required args
+
+    if (!Validate.IsDefined(this.args.src))
+      errors.push('IMAGE_PRIMITIVE_ERROR: Source is undefined.');
+    else {
+      if (!Validate.IsString(this.args.src))
+        errors.push('IMAGE_PRIMITIVE_ERROR: Source is not a string.');
+      else {
+        if (Validate.IsEmptyString(this.args.src))
+          errors.push('IMAGE_PRIMITIVE_ERROR: Source is empty string.');
+        else if (Validate.IsWhitespace(this.args.src))
+          errors.push('IMAGE_PRIMITIVE_ERROR: Source is whitespace.');
+      }
+    }
+
+    if (!Validate.IsDefined(this.corner))
+      errors.push('IMAGE_PRIMITIVE_ERROR: Corner is undefined.');
+    else {
+      if (this.args.corner.type == 'Coordinates')
+        errors.push('IMAGE_PRIMITIVE_ERROR: Corner is not a Coordinates object.');
+      else {
+        let errs = this.args.corner.Errors();
+        if (errs.length > 0)
+          errors.push(`IMAGE_PRIMITIVE_ERROR: Corner has errors: ${errs.join(' ')}`);
+      }
+    }
+
+    // Check optional args
+
+    let widthErrors = [];
+    let heightErrors = [];
+
+    let widthIsDefined = Validate.IsDefined(this.args.width);
+    let heightIsDefined = Validate.IsDefined(this.args.height);
+
+    if (widthIsDefined && heightIsDefined) {
+      let widthIsInteger = Validate.IsInteger(this.args.width);
+      let heightIsInteger = Validate.IsInteger(this.args.height);
+
+      if (widthIsInteger && heightIsInteger) {
+        if (this.args.width < params.width.min)
+          widthErrors.push(`IMAGE_PRIMITIVE_ERROR: Width is out of bounds. Assigned value is ${this.args.width}. Value must be greater than or equal to ${params.width.min}.`);
+
+        if (this.args.height < params.height.min)
+          heightErrors.push(`IMAGE_PRIMITIVE_ERROR: Height is out of bounds. Assigned value is ${this.args.height}. Value must be greater than or equal to ${params.height.min}.`);
+      }
+      else {
+        if (!widthIsInteger)
+          widthErrors.push('IMAGE_PRIMITIVE_ERROR: Width is not an integer.');
+
+        if (!heightIsInteger)
+          heightErrors.push('IMAGE_PRIMITIVE_ERROR: Height is not an integer.');
+      }
+
+      errors = errors.concat(widthErrors).concat(heightErrors);
+    }
+
+    return errors;
+  }
+
+  /**
+   * @override
+   */
+  static Parameters() {
+    return {
+      src: {
+        type: 'string'
+      },
+      corner: {
+        type: 'Coordinates'
+      },
+      width: {
+        type: 'number',
+        subtype: 'integer',
+        min: 1,
+        default: 0
+      },
+      height: {
+        type: 'number',
+        subtype: 'integer',
+        min: 1,
+        default: 0
+      }
+    };
   }
 }
 
 //-----------------------------------
 // EXPORTS
 
-exports.Create = Image.Create;
-exports.Name = 'Image';
-exports.Layer = false;
-exports.Consolidate = true;
-exports.Dependencies = null;
-exports.ComponentType = 'drawable';
+exports.Image = Image;
