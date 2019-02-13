1f3f9b4c91b46c9199091b0cdb41006b4d90da9f
Jesus Arevalo <isa90201@gmail.com>
2019-02-12 21:12:39 -0800

    Created a drawable base class.

---
+++ Drawables/drawablebaseclass.js
@@ -0,0 +1,31 @@
+let ObjectInterface = require('./objectinterface.js').ObjectInterface;
+
+//----------------------------------
+
+class DrawableBaseClass extends ObjectInterface {
+  constructor(properties) {
+    super({ category: 'drawable' });
+    this.type = properties.type;
+    this.name = properties.name;
+    this.args = properties.args;
+  }
+
+  /**
+   * @returns {boolean} Returns true if the drawable object can be rendered on it's own. False otherwise.
+   */
+  static IsLayer() {
+    // override
+  }
+
+  /**
+   * @returns {boolean} Returns true if the drawable object can be consolidated with other. False otherwise.
+   */
+  static IsConsolidatable() {
+    // override
+  }
+}
+
+//------------------------------
+// EXPORTS
+
+exports.DrawableBaseClass = DrawableBaseClass;
