205810b1b1ff380c86fa5dad543110087db1d7ba
Jesus Arevalo <isa90201@gmail.com>
2019-03-04 16:14:59 -0800

    Created a base class for all mod modules.

---
+++ im_modules/Drawables/Effects/Mod/modbaseclass.js
@@ -0,0 +1,24 @@
+let Path = require('path');
+let RootDir = Path.resolve('.');
+let Filepath = require(Path.join(RootDir, 'filepath.js')).Filepath;
+let EffectBaseClass = require(Path.join(Filepath.EffectsDir(), 'effectbaseclass.js')).EffectBaseClass;
+
+//-----------------------------
+
+class ModBaseClass extends EffectBaseClass {
+  constructor(properties) {
+    super({
+      name: properties.name,
+      args: properties.args,
+      offset: properties.offset,
+      subtype: 'mod'
+    });
+
+    this.command = properties.command;
+  }
+}
+
+//--------------------------------
+// EXPORTS
+
+exports.ModBaseClass = ModBaseClass;
