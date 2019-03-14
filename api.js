81d848d53e70b227c301f2160fd45f37ddf8f576
Jesus Arevalo <isa90201@gmail.com>
2019-03-14 14:25:04 -0700

    Updated Aura effect in API .

--- api.js
+++ api.js
@@ -193,7 +193,7 @@ Api.Drawables = {
   },
   Effects: {
     Fx: {
-      Aura: require(Path.join(Filepath.FxDir(), 'aura.js')).Aura,
+      Aura: require(Path.join(Filepath.SpecialCommandDir(), 'aura.js')).Aura,
       Blur: require(Path.join(Filepath.FxDir(), 'blur.js')).Blur,
       CharcoalSketch: require(Path.join(Filepath.FxDir(), 'charcoalsketch.js')).CharcoalSketch,
       ColoringBookSketch: require(Path.join(Filepath.FxDir(), 'coloringbooksketch.js')).ColoringBookSketch,
