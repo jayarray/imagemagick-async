7de9cb0ac81bbf5f1253e3768f83cfd2f16df60b
Jesus Isaias Arevalo <isa90201@gmail.com>
2019-06-20 09:34:47 -0700

    Created the special renderer base class to establish an interface for all special renderers to use.

---
+++ Render/specialrendererbaseclass.js
@@ -0,0 +1,13 @@
+class SpecialRendererBaseClass {
+  constructor() {
+  }
+
+  Render(layer, outputDir, format) {
+    // Override
+  }
+}
+
+//---------------------------
+// EXPORTS
+
+exports.SpecialRendererBaseClass = SpecialRendererBaseClass;
