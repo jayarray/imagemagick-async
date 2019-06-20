4a2976630e5cee9f74f371c03ccc927ba12e8a12
Jesus Isaias Arevalo <isa90201@gmail.com>
2019-06-20 11:37:54 -0700

    Created special procedure renderer module for handling renders that require user-defined processes.

---
+++ Render/specialprocedurerenderer.js
@@ -0,0 +1,45 @@
+let Path = require('path');
+let PathParts = __dirname.split(Path.sep);
+let index = PathParts.indexOf('imagemagick-async');
+let RootDir = PathParts.slice(0, index + 1).join(Path.sep);
+let Filepath = require(Path.join(RootDir, 'filepath.js')).Filepath;
+let Guid = require(Path.join(Filepath.LayerDir(), 'guid.js'));
+
+let SpecialRendererBaseClass = require(Path.join(Filepath.RenderDir(), 'specialrendererbaseclass.js')).SpecialRendererBaseClass;
+let LinuxCommands = require('linux-commands-async');
+
+//--------------------------------------
+
+class SpecialProcedureRenderer extends SpecialRendererBaseClass {
+  constructor() {
+  }
+
+  Render(layer, outputDir, format) {
+    return new Promise((resolve, reject) => {
+      let filename = Guid.Filename(Guid.DEFAULT_LENGTH, format);
+      let outputPath = Path.join(outputDir, filename);
+
+      let foundation = layer.args.foundation;
+      let desiredDest = foundation.args.dest;
+
+      // Temporarily change the output path
+      foundation.args.dest = outputPath;
+
+      foundation.Render().then(success => {
+
+        // Restore dest
+        foundation.args.dest = desiredDest;
+
+        // Move file to desired dest
+        LinuxCommands.Move.Move(outputPath, desiredDest, LinuxCommands.Command.LOCAL).then(success => {
+          resolve(desiredDest);
+        }).catch(error => `Failed to move special procedure render: ${error}`);
+      }).catch(error => `Failed to render special procedure render: ${error}`);
+    });
+  }
+}
+
+//------------------------------
+// EXPORTS
+
+exports.SpecialProcedureRenderer = SpecialProcedureRenderer;
