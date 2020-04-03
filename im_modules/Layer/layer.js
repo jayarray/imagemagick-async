bafc67085174acb91d03e13325c35af6401840f7
Jesus Isaias Arevalo <isa90201@gmail.com>
2020-04-03 13:28:31 -0700

    Refactored Layer module.

--- im_modules/Layer/layer.js
+++ im_modules/Layer/layer.js
@@ -10,6 +10,203 @@ let Filepath = require(Path.join(RootDir, 'filepath.js')).Filepath;
 let Offset = require(Path.join(Filepath.InputsDir(), 'offset.js')).Offset;
 let GravityValues = require(Path.join(Filepath.ConstantsDir(), 'gravity.json')).values;
 let ObjectInterface = require(Path.join(RootDir, 'objectinterface.js')).ObjectInterface;
+let Composite = require(Path.join(Filepath.ModComposeDir(), 'composite.js')).Composite;
+let Guid = require(Path.join(Filepath.LayerDir(), 'guid.js'));
+
+let LinuxCommands = require('linux-commands-async');
+let LocalCommand = LinuxCommands.Command.LOCAL;
+
+//-------------------------------------
+// HELPERS
+//-------------------------------------
+
+
+/**
+ * @param {string} extension The file type (i.e. png, jpeg, etc)
+ * @returns {string} Returns a random file name.
+ */
+function GetRandomFileName(extension) {
+  let name = Guid.Filename(Guid.DEFAULT_LENGTH, extension);
+  return name;
+}
+
+/**
+ * @param {string} filepath The file location
+ * @returns {string} Returns the file extension
+ */
+function GetFileExtension(filepath) {
+  let extension = LinuxCommands.Path.Extension(filepath).replace('.', '');
+  return extension;
+}
+
+/**
+ * @param {string} tempDir The full path to the temp dir
+ * @param {string} dest The output path
+ * @returns {string} Returns a generated filepath
+ */
+function GetTempFilepath(tempDir, dest) {
+  let extension = GetFileExtension(dest);
+  let filename = GetRandomFileName(extension);
+  let tempFilepath = Path.join(tempDir, filename);
+  return tempFilepath;
+}
+
+//--------------------------
+
+/**
+ * @param {string} imgPath The path to the image you will be drawing to.
+ * @param {Array<>} primitives A list of primitives you want to draw on the image.
+ * @param {string} dest The output destination path for the rendered image.
+ * @returns {Promise<string>} Returns a Promise that resolves if successful and returns the output path of the newly rendered image.
+ */
+function DrawPrimitives(imgPath, primitives, dest) {
+  return new Promise((resolve, reject) => {
+    if (!primitives || primitives.length == 0) {
+      resolve(dest);
+      return;
+    }
+
+    let cmd = 'convert';
+    let args = [imgPath];
+
+    primitives.forEach(p => {
+      args = args.concat(p.Args());
+    });
+
+    args.push(dest);
+
+    LocalCommand.Execute(cmd, args).then(output => {
+      if (output.stderr) {
+        reject(output.stderr);
+        return;
+      }
+
+      resolve(dest);
+    }).catch(error => reject(`Failed to draw primitives: ${error}`));
+  });
+}
+
+
+/**
+ * @param {string} imgPath The path to the image you will be drawing to.
+ * @param {Array<>} effects A list of effects you want to apply to the image.
+ * @param {string} dest The output destination path for the rendered image.
+ * @returns {Promise<string>} Returns a Promise with the output path of the newly rendered image.
+ */
+function ApplyEffects(imgPath, effects, dest) {
+  return new Promise((resolve, reject) => {
+
+    let applyEffectsInSequence = (source, fxArr, outputPath) => {
+      return new Promise((resolve, reject) => {
+        if (!fxArr || fxArr.length == 0) {
+          resolve(outputPath);
+          return;
+        }
+
+        let currEffect = fxArr[0];
+        let nextEffects = fxArr.slice(1);
+
+        if (currEffect.args.source1)
+          currEffect.args.source1 = source;
+        else if (currEffect.args.source)
+          currEffect.args.source = source;
+
+        currEffect.Render(outputPath).then(newOutputPath => {
+          resolve(applyEffectsInSequence(newOutputPath, nextEffects, newOutputPath));
+        }).catch(error => reject(error));
+      });
+    };
+
+    applyEffectsInSequence(imgPath, effects, dest).then(outputPath => {
+      resolve(outputPath);
+    }).catch(error => reject(error));
+  });
+}
+
+
+/**
+ * @param {Array<>} overlays A list of Layer objects.
+ * @param {string} parentDir The directory where you want to render the overlays.
+ * @param {string} extension The file extension type to render the image in.
+ * @returns {Promise<Array<{filepath: string, offset: object}>>} Returns a Promise with a list of objects.
+ */
+function RenderOverlays(overlays, parentDir, extension) {
+  return new Promise((resolve, reject) => {
+
+    // Create render actions for each overlay.
+
+    let offsetArr = [];
+    let actions = [];
+
+    overlays.forEach(o => {
+
+      // Get offset
+      offsetArr.push(o.args.offset);
+
+      // Get render action
+      let filename = GetRandomFileName(extension);
+      let dest = Path.join(parentDir, filename);
+      let a = o.Render(dest);
+      actions.push(a);
+    });
+
+    Promise.all(actions).then(results => {
+
+      let filepathOffsetPairs = [];
+
+      for (let i = 0; i < results.length; ++i) {
+        let currOverlay = overlays[i];
+        let currOffset = offsetArr[i];
+        let currFilepath = results[i];
+
+        let pair = {
+          filepath: currFilepath,
+          offset: currOffset
+        };
+
+        filepathOffsetPairs.push(pair);
+      }
+
+      resolve(filepathOffsetPairs);
+    }).catch(error => reject(error));
+  });
+}
+
+
+/**
+ * Execute the last step in rendering a layer.
+ * @param {string} foundationOutputPath The location for the rendered foundation
+ * @param {Array<{filepath: string, offset: object}>} filepathOffsetTuples A list of objects with properties 'filepath' and 'offset'.
+ * @param {string} gravity A valid string value for gravity
+ * @param {string} dest The output path of the render
+ * @returns {Promise<string>} Returns a Promise with the output path of the newly rendered image
+ */
+function CreateComposite(foundationOutputPath, filepathOffsetPairs, gravity, dest) {
+  return new Promise((resolve, reject) => {
+    let args = [foundationOutputPath];
+
+    if (gravity)
+      args.push('-gravity', gravity);
+
+    filepathOffsetPairs.forEach(t => {
+      let currFilepath = t.filepath;
+      let currOffset = t.offset;
+
+      args.push('-draw', `image over ${currOffset.args.x},${currOffset.args.y} 0,0 '${currFilepath}'`);
+    });
+
+    args.push(dest);
+
+    LocalCommand.Execute('convert', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to compose images: ${output.stderr}`);
+        return;
+      }
+
+      resolve(dest);
+    }).catch(error => `Failed to compose images: ${error}`);
+  });
+}
 
 //-------------------------------------
 
@@ -150,6 +347,64 @@ class Layer extends ObjectInterface {
       }
     }
     return new Builder();
+  }
+
+  /**
+   * @param {string} dest The desired output path for the render.
+   * @returns {Promise<string>} Returns a Promise with the output path for the newly rendered layer.
+   */
+  Render(dest) {
+    return new Promise((resolve, reject) => {
+
+      // Render foundation (canvas)
+      this.args.foundation.Render(dest).then(imgPath1 => {
+
+        // Apply primitives or effects (check order)
+
+        let firstAction = null;
+        let secondAction = null;
+
+        if (this.args.drawPrimitivesFirst) {
+          firstAction = DrawPrimitives(imgPath1, this.args.primitives, imgPath1);
+          secondAction = ApplyEffects(imgPath1, this.args.appliedEffects, imgPath1);
+        }
+        else {
+          firstAction = ApplyEffects(imgPath1, this.args.appliedEffects, imgPath1);
+          secondAction = DrawPrimitives(imgPath1, this.args.primitives, imgPath1);
+        }
+
+
+        firstAction.then(outputPath1 => {
+          secondAction.then(outputPath2 => {
+
+            // Render any overlays
+
+            let overlays = this.args.overlays;  // <- these are all Layer objects
+
+            if (!overlays || overlays.length == 0) {
+              resolve(outputPath2);
+              return;
+            }
+            else {
+              let parentDir = LinuxCommands.Path.ParentDir(outputPath2);
+              let extension = GetFileExtension(outputPath2);
+
+              RenderOverlays(overlays, parentDir, extension).then(filepathOffsetPairs => {
+                CreateComposite(outputPath2, filepathOffsetPairs, this.args.gravity, outputPath2).then(outputPath3 => {
+
+                  // Clean up overlay temp files
+
+                  let tempFilepaths = filepathOffsetPairs.map(x => x.filepath);
+                  LinuxCommands.Remove.Files(tempFilepaths, LocalCommand).then(success => {
+                    resolve(outputPath3);
+                  }).catch(error => reject(error));
+                }).catch(error => reject(error));
+              }).catch(error => reject(error));
+            }
+          }).catch(error => reject(error));
+        }).catch(error => reject(error));
+      }).catch(error => reject(error));
+    });
   }
 
   /**
